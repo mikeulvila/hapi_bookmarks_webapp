c84f7f38244b3a3bcf11abb50096cf796feb2ae5
Mike Ulvila <mikeulvila@me.com>
2016-08-03 13:59:29 -0500

    auth.strategy requires password value to be at least 32 characters long

--- plugins/auth.js
+++ plugins/auth.js
@@ -3,7 +3,7 @@
 exports.register = function(server, options, next) {
 
   server.auth.strategy('session', 'cookie', 'optional', {
-    password: 'eGenCG7wGdzeiKISE7Ftt2A7',
+    password: 'eGenCG7wGdzeiKISE7Ftt2A7jsAS39ja',
     isSecure: false
   });
 
